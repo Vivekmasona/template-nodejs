a640390b903632e765dbad7dd02749ac22651963
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-09-23 12:58:28 +0530

    Update app.js
    Signed-off-by: Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 const express = require('express');
+const ytdl = require('ytdl-core');
+const fs = require('fs');
 const app = express();
-const port = process.env.PORT ?? 3000;
+const port = 3000;
 
-app.use(express.static('public'))
+app.get('/download', async (req, res) => {
+  try {
+    const videoURL = req.query.url; // Get the YouTube video URL from the query parameter
 
-app.get('*', (req, res) => {
-    res.redirect('/');
-})
+    if (!videoURL) {
+      return res.status(400).send('Missing video URL');
+    }
+
+    // Get information about the video (including the title)
+    const info = await ytdl.getInfo(videoURL);
+    const videoTitle = info.videoDetails.title;
+    const autoTitle = videoTitle.replace(/[^\w\s]/gi, ''); // Remove special characters from the title
+    const sanitizedTitle = autoTitle || 'audio'; // Use the sanitized title or 'audio' as a default
+
+    // Set response headers to specify a downloadable file with the auto-generated title
+    res.setHeader('Content-Disposition', `attachment; filename="${sanitizedTitle}.mp3"`);
+    res.setHeader('Content-Type', 'audio/mpeg');
+
+    // Pipe the audio stream into the response
+    ytdl(videoURL, { filter: 'audioonly' }).pipe(res);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
